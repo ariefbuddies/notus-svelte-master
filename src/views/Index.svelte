edcb2257015030e7783d010b460318992a1df156
Arief Buddies <54927162+ariefbuddies@users.noreply.github.com>
2020-12-15 20:00:48 +0700

    Update Index.svelte

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -574,66 +574,7 @@
   </div>
 </section>
 
-<section class="block relative z-1 bg-gray-700">
-  <div class="container mx-auto">
-    <div class="justify-center flex flex-wrap">
-      <div class="w-full lg:w-12/12 px-4 -mt-24">
-        <div class="flex flex-wrap">
-          <div class="w-full lg:w-4/12 px-4">
-            <h5 class="text-xl font-semibold pb-4 text-center">
-              Login Page
-            </h5>
-            <Link to="/auth/login">
-              <div
-                class="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150"
-              >
-                <img
-                  alt="..."
-                  class="align-middle border-none max-w-full h-auto rounded-lg"
-                  src="{login}"
-                />
-              </div>
-            </Link>
-          </div>
 
-          <div class="w-full lg:w-4/12 px-4">
-            <h5 class="text-xl font-semibold pb-4 text-center">
-              Profile Page
-            </h5>
-            <Link to="/profile">
-              <div
-                class="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150"
-              >
-                <img
-                  alt="..."
-                  class="align-middle border-none max-w-full h-auto rounded-lg"
-                  src="{profile}"
-                />
-              </div>
-            </Link>
-          </div>
-
-          <div class="w-full lg:w-4/12 px-4">
-            <h5 class="text-xl font-semibold pb-4 text-center">
-              Landing Page
-            </h5>
-            <Link to="/landing">
-              <div
-                class="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150"
-              >
-                <img
-                  alt="..."
-                  class="align-middle border-none max-w-full h-auto rounded-lg"
-                  src="{landing}"
-                />
-              </div>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
 
 <section class="py-20 bg-gray-700 overflow-hidden">
   <div class="container mx-auto pb-64">
