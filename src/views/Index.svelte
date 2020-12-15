e8423473bc19c19e4025c9c2c0a45a6bbfb907e2
Arief Buddies <54927162+ariefbuddies@users.noreply.github.com>
2020-12-15 19:47:50 +0700

    Update Index.svelte

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -347,51 +347,6 @@
   </div>
 
 
-<section class="py-20 bg-gray-700 overflow-hidden">
-  <div class="container mx-auto pb-64">
-    <div class="flex flex-wrap justify-center">
-      <div class="w-full md:w-5/12 px-12 md:px-4 ml-auto mr-auto md:mt-64">
-        <div
-          class="text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white"
-        >
-          <i class="fas fa-code-branch text-xl"></i>
-        </div>
-        <h3 class="text-3xl mb-2 font-semibold leading-normal text-white">
-          Open Source
-        </h3>
-        <p class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-500">
-          Since
-          <a
-            href="https://tailwindcss.com/?ref=creativetim"
-            class="text-gray-400"
-            target="_blank"
-          >
-            Tailwind CSS
-          </a>
-          is an open source project we wanted to continue this movement too. You
-          can give this version a try to feel the design and also test the
-          quality of the code!
-        </p>
-        <p class="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-500">
-          Get it free on Github and please help us spread the news with a Star!
-        </p>
-        <a
-          href="https://github.com/creativetimofficial/notus-svelte?ref=ns-index"
-          target="_blank"
-          class="github-star mt-4 inline-block text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-gray-800 active:bg-gray-700 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
-        >
-          Github Star
-        </a>
-      </div>
-
-      <div class="w-full md:w-4/12 px-4 mr-auto ml-auto mt-32 relative">
-        <i
-          class="fab fa-github text-gray-800 text-55 absolute -top-150-px -right-100 left-auto opacity-80"
-        ></i>
-      </div>
-    </div>
-  </div>
-</section>
 <section class="pb-16 bg-gray-300 relative pt-32">
   <div
     class="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20"
@@ -453,4 +408,4 @@
     </div>
   </div>
 </section>
-<Footer />
+<Footer/>
