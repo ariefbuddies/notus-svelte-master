916ce30e66047c04801dae9a30cc40090d193686
Arief Buddies <54927162+ariefbuddies@users.noreply.github.com>
2020-12-15 14:28:51 +0700

    Update IndexDropdown.svelte

--- src/components/Dropdowns/IndexDropdown.svelte
+++ src/components/Dropdowns/IndexDropdown.svelte
@@ -99,7 +99,7 @@
       href="/landing"
       class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
     >
-      Lading
+      Landing
     </a>
     <a
       use:link
