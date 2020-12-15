564e558f9c1b5519968f655305a9cdb2ed5aa683
Arief Buddies <54927162+ariefbuddies@users.noreply.github.com>
2020-12-15 19:48:49 +0700

    Update Index.svelte

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -3,7 +3,7 @@
 
   // core components
   import IndexNavbar from "components/Navbars/IndexNavbar.svelte";
-  import Footer from "components/Footers/Footer.svelte";
+  // import Footer from "components/Footers/Footer.svelte";
 
   const patternVue = "/assets/img/13.png";
   const componentBtn = "/assets/img/component-btn.png";
