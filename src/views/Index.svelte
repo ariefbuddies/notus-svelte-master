1cc3a352bca8f2963589146658604eb58d858593
Arief Buddies <54927162+ariefbuddies@users.noreply.github.com>
2020-12-16 21:50:48 +0700

    Update Index.svelte

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -7,11 +7,13 @@
 
   const patternVue = "/assets/img/13.png";
   const componentBtn = "/assets/img/component-btn.png";
+  const componentProfileCard = "/assets/img/component-profile-card.png";
   const componentInfoCard = "/assets/img/component-info-card.png";
   const componentInfo2 = "/assets/img/component-info-2.png";
   const componentMenu = "/assets/img/component-menu.png";
   const componentBtnPink = "/assets/img/component-btn-pink.png";
   const documentation = "/assets/img/documentation.png";
+  const login = "/assets/img/login.jpg";
   const profile = "/assets/img/profile.jpg";
   const landing = "/assets/img/landing.jpg";
   export let location;
