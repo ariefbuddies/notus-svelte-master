c888e9007d5037b60eac8ba4e73f774bd60ac132
Arief Buddies <54927162+ariefbuddies@users.noreply.github.com>
2020-12-16 22:23:57 +0700

    Update Index.svelte

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -278,7 +278,6 @@
 <div class="w-full md:w-4/12 px-12 md:px-4 ml-auto mr-auto mt-48">
 </div>
     </div>
-  </div>
 <div class="justify-center text-center flex flex-wrap mt-24">
     <div class="w-full md:w-6/12 px-12 md:px-4">
       <h2 class="font-semibold text-4xl">Let's svelte!!</h2>
